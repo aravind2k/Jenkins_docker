7602542a3f5fdcca0de32b03b7817d53365b3617
aravind2k <aravindprasath652@gmail.com>
2022-02-26 00:48:23 +0530

    check auto build

--- src/main/java/com/example/Jenkinsdocker/JenkinsDockerApplication.java
+++ src/main/java/com/example/Jenkinsdocker/JenkinsDockerApplication.java
@@ -8,6 +8,7 @@ public class JenkinsDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerApplication.class, args);
+		System.out.println("Auto build worked");
 	}
 
 }
